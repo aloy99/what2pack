7295c5475c3d938ad981824688dc2a8ec632de22
Christine Qiu <cqiu@kth.se>
2023-03-28 12:42:27 +0200

    finished basic model

--- src/reactjs/startPresenter.jsx
+++ src/reactjs/startPresenter.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import StartView from '../views/startView.jsx';
 
 function StartPresenter(props){
-    return <StartView model={props.model}/>;
+    function handleSearchInputACB(destination, startDate, endDate){
+        const plan = {destination: destination, startDate: startDate, endDate: endDate};
+        props.model.setCurrentPlan(plan);
+        props.model.addPlan(plan);
+        console.log(props.model);
+    }
+    function handleDestACB(dest){
+        props.model.setSearchDestination(dest);
+    }
+    function handleRangeACB(startDate, endDate){
+        props.model.setSearchDateRange(startDate, endDate);
+    }
+    return <StartView   
+            model={props.model} 
+            onSearchInput={handleSearchInputACB}
+            onDestChanged={handleDestACB}
+            onRangeChanged={handleRangeACB}/>;
 }
 
 export default StartPresenter;
