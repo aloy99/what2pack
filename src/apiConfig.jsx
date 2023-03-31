5493a7848823eaead310605efd5f12a96519c743
unknown <achc@kth.se>
2023-03-31 23:36:28 +0200

    Fixed script

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,5 +1,5 @@
 
-const GMAPS_BASE_URL = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places'
+const GMAPS_BASE_URL = 'https://maps.googleapis.com/maps/api/js?key='
 const GMAPS_API_KEY = ''
 
 const HOLIDAYS_BASE_URL = ''
