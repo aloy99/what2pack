2d63435588f8d92753ebd04bce48d0db9652827a
xFleur <mels1999@live.nl>
2023-04-17 19:00:49 +0200

    auth presenter and view seperate consern

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import StartPresenter from './reactjs/startPresenter.jsx';
-import LoginPresenter from './reactjs/loginPresenter.jsx';
-import SignUpPresenter from './reactjs/signUpPresenter.jsx';
 import DetailsPresenter from './reactjs/detailsPresenter.jsx';
 import ProfilePresenter from './reactjs/profilePresenter.jsx';
 import What2PackModel from './What2PackModel';
 import './App.css'
+import AuthPresenter from './reactjs/authPresenter.jsx';
 
 const myModel = new What2PackModel();
 
@@ -16,8 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<StartPresenter model={myModel}/>}></Route>
         <Route path='/start' element={<StartPresenter model={myModel}/>}></Route>
-        <Route path='/login' element={<LoginPresenter model={myModel}/>}></Route>
-        <Route path='/signup' element={<SignUpPresenter model={myModel}/>}></Route>
+        <Route path='/login' element={<AuthPresenter model={myModel}/>}></Route>
         <Route path='/details' element={<DetailsPresenter model={myModel}/>}></Route>
         <Route path='/profile' element={<ProfilePresenter model={myModel}/>}></Route>
       </Routes>
---
+++ src/views/authView.jsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+function AuthView(props){
+    const [isSignUpShown, setIsSignUpShown] = useState(false);
+    const [isSignInShown, setIsSignInShown] = useState(true);
+
+    const navigate = useNavigate();
+
+    function changeEmailACB(e){
+        props.onEmailChange(e.target.value)
+      }
+    function changePasswordACB(e){
+        props.onPasswordChange(e.target.value)
+    }  
+
+    const handleSignUpClick = event => {
+        setIsSignUpShown(true);
+        setIsSignInShown(false);
+        // navigate('/profile')
+      };
+    const handleSignInClick = event => {
+        // setIsShown(current => !current);
+        // 👇️ or simply set it to true
+        setIsSignInShown(true);
+        setIsSignUpShown(false);
+        // navigate('/profile')
+    };
+
+
+
+    return (
+        <div className="App">
+            {isSignInShown && (
+                <div className="sign-in-container">
+                    <form  onSubmit={props.onUserSignIn}>
+                        <h1>Log In to account</h1>
+                        <p >No account yet?<button onClick={handleSignUpClick}>Sign Up here</button></p>
+                        <input type="email" placeholder="Enter your email" 
+                            value={props.email}
+                            onChange={changeEmailACB}  
+                        ></input>
+                        <input type="password" placeholder="Enter your password" 
+                            value={props.password}
+                            onChange={changePasswordACB}  
+                        ></input>
+                        <button type="submit" >Log In</button>
+                    </form>
+                </div>
+            )}
+
+            {isSignUpShown && (
+                <div className="sign-up-container">
+                    <form  onSubmit={props.onUserSignUp}>
+                        <h1>Create account</h1>  
+                        <p >All ready have an account? <button onClick={handleSignInClick}>Log In here</button></p> 
+                        <input type="email" placeholder="Enter your email" 
+                            value={props.email}
+                            onChange={changeEmailACB}   
+                        ></input>
+                        <input type="password" placeholder="Enter your password" 
+                            value={props.password}
+                            onChange={changePasswordACB} 
+                        ></input>
+                        <button type="submit" >Sign Up</button>
+                    </form>
+                </div>
+            )}
+        </div>
+    );
+}  
+export default AuthView;
