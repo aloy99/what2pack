64a908b59ae336f8729982422013ca52f02fe483
xFleur <mels1999@live.nl>
2023-04-20 14:22:56 +0200

    update apiConfig local

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,9 +1,9 @@
 
 const GMAPS_BASE_URL = 'https://maps.googleapis.com/maps/api/js?key='
-const GMAPS_API_KEY = ''
+const GMAPS_API_KEY = 'AIzaSyDg0YBH6lccvpjpfcj5qQcXz5asA2cdYCM'
 
-const HOLIDAYS_BASE_URL = ''
-const HOLIDAYS_API_KEY = ''
+const HOLIDAYS_BASE_URL = 'https://api.api-ninjas.com/v1/holidays?'
+const HOLIDAYS_API_KEY = 'q/POWdJvCFOA3JDShOcFvw==cLVCpVtBH36TVh8p'
 
 const WEATHER_BASE_URL = 'https://api.open-meteo.com/v1/forecast?'
 
