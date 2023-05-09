9fc819be959501f70ea62a42891485313b093c04
Christine Qiu <cqiu@kth.se>
2023-05-09 18:27:29 +0200

    add delete card

--- src/views/plansView.jsx
+++ src/views/plansView.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { Card } from 'antd';
+import { EditOutlined, CloseOutlined } from '@ant-design/icons';
+import useRerender from "../reactjs/useRerender.jsx";
 const { Meta } = Card;
 function PlansView(props){
+    const rerenderACB = useRerender();
     const navigate = useNavigate();
     function renderPlanCB(plan){
         const description = plan.startDate + " - " + plan.endDate;
@@ -10,13 +13,24 @@ function PlansView(props){
             navigate("/details");
             props.onCurrentPlanChanged(plan);
         }
+        function clickEditCardACB(){
+            clickCardACB();
+        }
+        function clickDeleteCardACB(){
+            console.log("delete card", plan);
+            props.onDeletePlan(plan);
+            rerenderACB();
+        }
         return (
             <Card
                 className='card-plan-profile'
                 key={plan.destination+" "+plan.startDate+" "+plan.endDate}
                 hoverable
-                cover={<img className='img-card-plan-profile' alt={plan.destination} src={plan.image} />}
-                onClick={clickCardACB}
+                cover={<img className='img-card-plan-profile' alt={plan.destination} src={plan.image} onClick={clickCardACB}/>}
+                actions={[
+                    <EditOutlined key="edit" onClick={clickEditCardACB}/>,
+                    <CloseOutlined key="ellipsis" onClick={clickDeleteCardACB}/>
+                ]}
             >
                 <Meta title={plan.destination} description={description} />
             </Card>
